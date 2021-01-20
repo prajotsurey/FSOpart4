f23bf6fce4fb9a3e7790a7f90692c6cd0429d124
prajotsurey <prajotsurey@gmail.com>
2021-01-20 18:31:04 +0530

    refactored mongoose schema

--- index.js
+++ index.js
@@ -4,16 +4,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
+const Blog = require('./models/blog')
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
