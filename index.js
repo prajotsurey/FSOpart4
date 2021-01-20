69ebb576c71c76fb7a539adfb58cb593d8adbc61
prajotsurey <prajotsurey@gmail.com>
2021-01-20 19:07:32 +0530

    added logger

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 const http = require('http')
 const express = require('express')
 const app = express()
@@ -7,7 +8,12 @@ const mongoose = require('mongoose')
 const notesRouter = require('./controllers/blogs')
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB', error.message)
+  })
 app.use(cors())
 app.use(express.json())
 
