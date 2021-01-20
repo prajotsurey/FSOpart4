9904a053dd982235eb8b5c805373a22d0621b1f5
prajotsurey <prajotsurey@gmail.com>
2021-01-20 19:29:10 +0530

    added app.js and refactored index.js

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
+const app = require('./app') // the actual Express application
+const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const notesRouter = require('./controllers/blogs')
-const middleware = require('./utils/middleware')
-const { errorHandler } = require('./utils/middleware')
 
+const server = http.createServer(app)
 
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB', error.message)
-  })
-app.use(cors())
-app.use(express.json())
-app.use(middleware.requestLogger)
-app.use('/api/blogs', notesRouter)
-
-app.use(middleware.unknownEndpoint)
-app.use(middleware.errorHandler)
-
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
