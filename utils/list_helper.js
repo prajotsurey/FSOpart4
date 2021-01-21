d242182c79c03d7ff6ec39ed171a0aa7416424ea
prajotsurey <prajotsurey@gmail.com>
2021-01-21 15:53:32 +0530

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,26 @@ const totalLikes = (blogs) => {
     : blogs.reduce( (sum, blog) => sum + blog.likes, 0 )
 }
 
+const favouriteBlog = (blogs) => {
+  const max = (highest, blog) => {
+    return highest.likes && highest.likes>= blog.likes
+      ? {
+        title: highest.title,
+        author: highest.author,
+        likes: highest.likes 
+      }
+      : {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes
+      }
+  } 
+  return blogs.reduce(max,{})
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favouriteBlog
 }
+
